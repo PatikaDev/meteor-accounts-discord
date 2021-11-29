18f64e340857852df4e4fb0b99ebbd4e47a39645
pisacode <mckiranli@gmail.com>
2021-11-29 22:30:56 +0300

    Update package.js

--- package.js
+++ package.js
@@ -1,30 +1,26 @@
 Package.describe({
-  name: 'lichthagel:accounts-discord',
-  version: '0.2.0',
+  name: "lichthagel:accounts-discord",
+  version: "0.2.0",
   // Brief, one-line summary of the package.
-  summary: 'Adds account support for Discord',
+  summary: "Adds account support for Discord",
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/Lichthagel/meteor-accounts-discord',
+  git: "https://github.com/Lichthagel/meteor-accounts-discord",
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+  documentation: "README.md",
 });
 
 Package.onUse(function (api) {
-  api.use('ecmascript@0.12.4');
-  api.use('accounts-base@1.4.3', ['client', 'server']);
+  api.use("ecmascript@0.12.4");
+  api.use("accounts-base@2.0.0", ["client", "server"]);
   // Export Accounts (etc) to packages using this one.
-  api.imply('accounts-base', ['client', 'server']);
+  api.imply("accounts-base", ["client", "server"]);
 
-  api.use('accounts-oauth@1.1.16', ['client', 'server']);
-  api.use('lichthagel:discord-oauth@0.1.0');
-  api.imply('lichthagel:discord-oauth');
+  api.use("accounts-oauth@1.1.16", ["client", "server"]);
+  api.use("lichthagel:discord-oauth@0.1.0");
+  api.imply("lichthagel:discord-oauth");
 
-  api.use(
-    ['accounts-ui@1.3.1', 'lichthagel:discord-config-ui@0.1.0'],
-    ['client', 'server'],
-    { weak: true }
-  );
-  api.addFiles('notice.js');
-  api.addFiles('discord.js');
+  api.use(["accounts-ui@1.3.1", "lichthagel:discord-config-ui@0.1.0"], ["client", "server"], { weak: true });
+  api.addFiles("notice.js");
+  api.addFiles("discord.js");
 });
